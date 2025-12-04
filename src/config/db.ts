25af372b462d6ff0095ff5a4d165c20707305754
EMON <jakariachowdhury2427@gmail.com>
2025-12-05 01:33:46 +0600

    refactor: update database connection and initialization logic

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,26 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-dotenv.config();
+
+dotenv.config(//{path: path.join(process.cwd() , '.env')}
+);
 
 
-const pool = new Pool({
-connectionString: `${process.env.DATABASE_URL}`
+export const pool = new Pool({
+connectionString: `postgresql://neondb_owner:npg_PIACEG4t7Rpz@ep-patient-mouse-a42oib0g-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
 });
 
+const connectDB = async () => {
+  
+    await pool.query(`CREATE TABLE IF NOT EXISTS users (
+        id AUTO GENERATED PRIMARY KEY,
+        name VARCHAR(100) REQUIRED NOT NULL,
+        email VARCHAR(100)  REQUIRED UNIQUE  LOWERCASE NOT NULL,
+        password VARCHAR(100) REQUIRED MIN 6 CHAR NOT NULL,
+        phone VARCHAR(15) REQUIRED NOT NULL,
+        role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer')) DEFAULT 'customer',
+        )`);
+    
+    };      
 
-export default pool;
+
+export default connectDB;
