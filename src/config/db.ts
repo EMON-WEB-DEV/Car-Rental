e36c29de4c89660626d43b4f212ed84f8a19d024
EMON <jakariachowdhury2427@gmail.com>
2025-12-05 02:09:11 +0600

    refactor: update user table schema and remove commented-out code

--- src/config/db.ts
+++ src/config/db.ts
@@ -12,14 +12,35 @@ connectionString: `postgresql://neondb_owner:npg_PIACEG4t7Rpz@ep-patient-mouse-a
 const connectDB = async () => {
   
     await pool.query(`CREATE TABLE IF NOT EXISTS users (
-        id AUTO GENERATED PRIMARY KEY,
-        name VARCHAR(100) REQUIRED NOT NULL,
-        email VARCHAR(100)  REQUIRED UNIQUE  LOWERCASE NOT NULL,
-        password VARCHAR(100) REQUIRED MIN 6 CHAR NOT NULL,
-        phone VARCHAR(15) REQUIRED NOT NULL,
-        role VARCHAR(20) NOT NULL CHECK (role IN ('admin', 'customer')) DEFAULT 'customer',
-        )`);
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        age INT NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        address TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
     
+  /*  await pool.query(`CREATE TABLE IF NOT EXISTS vehicles (
+    id  PRIMARY KEY,
+    vehicle_name VARCHAR(100) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    registration_number VARCHAR(50) NOT NULL UNIQUE,
+    daily_rental_price NUMERIC NOT NULL,
+    availability_status BOOLEAN DEFAULT TRUE
+)`);
+
+      await pool.query(`
+        CREATE TABLE IF NOT EXISTS bookings (
+    id  PRIMARY KEY,
+    customer_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    vehicle_id UUID REFERENCES vehicles(id) ON DELETE CASCADE,
+    rent_start_date DATE NOT NULL,
+    rent_end_date DATE NOT NULL,
+    total_price NUMERIC NOT NULL,
+    status VARCHAR(20) DEFAULT 'pending' CHECK (status IN ('pending','confirmed','cancelled','completed'))
+)`)*/
+
     };      
 
 
